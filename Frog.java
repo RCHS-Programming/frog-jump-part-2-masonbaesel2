94196fe9ab6c1da1e17576e9286a8188436954b6
masonbaesel2 <75792384+masonbaesel2@users.noreply.github.com>
2021-04-23 08:16:08 -0400

    changed code

--- Frog.java
+++ Frog.java
@@ -46,17 +46,40 @@ public class Frog extends Actor
         }
         
         //Check for the down key
-        
+        if( isDownPress == false && Greenfoot.isKeyDown("Down") && getY() < 540)
+        {
+            setLocation( getX() , getY() + 60);
+            isDownPress = true;
+        }
+        if( isDownPress && !Greenfoot.isKeyDown("down"))
+        {
+            isDownPress = false;
+        }
         
         
         
         //Check for the right key
-        
-        
+        if( isRightPress == false && Greenfoot.isKeyDown("right") && getX() < 660)
+        {
+            setLocation( getX()+60 , getY());
+            isRightPress = true;
+        }
+        if( isRightPress && !Greenfoot.isKeyDown("right"))
+        {
+            isRightPress = false;
+        }
         
         
         //Check for the left key
-        
+        if( isLeftPress == false && Greenfoot.isKeyDown("left") && getX() > 60)
+        {
+            setLocation( getX()-60 , getY());
+            isLeftPress = true;
+        }
+        if( isLeftPress && !Greenfoot.isKeyDown("left"))
+        {
+            isLeftPress = false;
+        }
         
         
         
